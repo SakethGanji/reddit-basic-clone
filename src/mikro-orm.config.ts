1f16fa36a09fec152390d15bc2249e3be9dcd638
Saketh Ganji <ganjisaketh@gmail.com>
2022-06-19 22:09:03 +0530

    set up user auth

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,16 +2,17 @@ import { Post } from "./entities/Post";
 import { __prod__ } from "./constants";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
+import {User} from "./entities/User";
 
 export default {
   migrations: {
     path: path.join(__dirname, "./migrations"),
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
-  entities: [Post],
-  dbName: "lireddit",
-  type: "postgresql",
-  user: "postgres",
-  password:"Ankith0926!",
+  entities: [Post, User],
+  dbName: "",
+  type: "",
+  user: "",
+  password:"!",
   debug: !__prod__
 } as Parameters<typeof MikroORM.init>[0];
